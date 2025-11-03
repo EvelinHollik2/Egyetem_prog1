8b202c9286854282dbfcf708565bc137d300631e
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2025-11-03 09:25:16 +0100

    Add files via upload

--- 25-26/8_2fel.py
+++ 25-26/8_2fel.py
@@ -1,20 +1,20 @@
-import math
 import random
+import math
 
 #feltöltünk egy listát 10 darab random számmal 1-100-ig
 def feltolt(lista:list) -> None:
     for i in range(10):
         lista.append(random.randint(1, 100))
 
-#kiirajtuk a listából a legkisebb számot
+#visszatér a lista legkisebb elemével
 def legkisebb(lista:list) -> int:
-    min = lista[0]
+    mini = lista[0]
     for i in range(len(lista)):
-        if lista[i] < min:
-            min = lista[i]
-    return min
+        if lista[i] < mini:
+            mini = lista[i]
+    return mini
 
-#kiirajtuk a listából a legnagyobb számot
+#visszatér a lista legnagyobb elemének az indexével
 def legnagyobb_index(lista:list)->int:
     maxi = 0
     for i in range(len(lista)):
@@ -22,7 +22,7 @@ def legnagyobb_index(lista:list)->int:
             maxi = 1
     return maxi
 
-#kiirajtuk a listából a páros elemek számát
+#visszatér a lista páros eleneinek számával
 def paros_elem(lista:list)->int:
     db = 0
     for i in range(len(lista)):
@@ -30,7 +30,7 @@ def paros_elem(lista:list)->int:
             db += 1
     return db
 
-#megnézzük hogy prím e
+#visszaadja, hogy egy egész szám prím vagy sem (True/False)
 def prim_e(szam: int) -> bool:
     if szam == 0 or szam == 1:
         return False
@@ -41,7 +41,7 @@ def prim_e(szam: int) -> bool:
         o += 1
     return True
 
-#prím számok elemeinek száma
+#visszatér a listában levő prímek számával
 def primek_szama(lista:list) -> int:
     prim_db = 0
     for i in range(len(lista)):
@@ -49,7 +49,6 @@ def primek_szama(lista:list) -> int:
             prim_db += 1
     return prim_db
 
-#függvény ellenőrzés
 def main():
     lista=[]
     feltolt(lista)
